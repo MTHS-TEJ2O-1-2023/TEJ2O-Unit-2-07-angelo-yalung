603a7bb8b8d3a221b8a139643177fc6230911879
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-05 13:47:58 +0000

    rt

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,20 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Angelo Yalung
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+Number_Of_Cookies = 0
+
+input.onButtonPressed(button_a)
+Number_Of_Cookies = Number_Of_Cookies + 1
+display.show(Number_Of_Cookies)
+display.show(str(Number_Of_Cookies))
+
+input.onButtonPressed(button_b)
+NumberOfCookies = 0
+display.show(Number_Of_Cookies)
+display.show(str(Number_Of_Cookies))
--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Angelo Yalung
- * Created on: Sep 2023
- * This program ...
+ * Created on: Oct 2023
+ * This program turns the microbit into Cookie Clicker
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+
+// variables
+let numberOfCookies: number
+numberOfCookies = 0
+
+// on "A" button pressed
+input.onButtonPressed(Button.A, function () {
+  numberOfCookies = numberOfCookies + 1
+  basic.showNumber(numberOfCookies)
+})
+
+// on "B" button pressed
+input.onButtonPressed(Button.B, function () {
+  numberOfCookies = 0
+  basic.showNumber(numberOfCookies)
+})
